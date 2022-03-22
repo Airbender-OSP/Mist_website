44cacf2fd9b4a92b6c940ec0123bf7fd50aacf26
anthonymarinated <anthonymarin718@gmail.com>
2022-03-21 20:01:27 -0400

    wiptransitions

--- mistWebsite/src/components/Home.svelte
+++ mistWebsite/src/components/Home.svelte
@@ -2,11 +2,84 @@
   import About from './About.svelte';
   import Features from './Features.svelte';
   import Team from './Team.svelte';
+  import { fade, fly, scale, blur, slide } from 'svelte/transition';
+  import { circIn } from 'svelte/easing';
+
+  let y;
+  $:console.log(y);
+  let modalOpen = true;
 </script>
 
-<p>Home</p>
-<About />
-<Features />
-<Team />
+<svelte:window bind:scrollY={y} />
 
-<style></style>
+<p>Home</p>
+<div class="About">
+  <About />
+</div>
+
+{#if y > 105}
+  <!-- <div transition:fade={{ opacity: 0.1 }}>
+  <Features /> -->
+  <div class="features" transition:fade={{ opacity: 0.1 }}>
+    <h1>Features</h1>
+  
+    <h2>Start Button</h2>
+    <p>
+      Start button 'on click' sends a fetch request to the user's linked SQL
+      database. The fetch request sends a custom query to get the users current
+      session number.
+      <img src="../public/assets/dog.png" alt="Anthony" style="width:100%" />
+    </p>
+  
+    <h2>Stop Button</h2>
+    <p>
+      Stop Button 'on click' sends a SQL query that gathers all metric data from
+      the users metric table in their SQL database where the session number is
+      equal to the current session number.
+    </p>
+  
+    <h2>Generate Metrics</h2>
+    <p>Track your metrics with the click of a button!</p>
+  
+    <h2>Clear Metrics</h2>
+    
+  </div>
+  <!-- </div> -->
+{/if}
+
+<!-- {#if y > 400} -->
+<div>
+  <Team />
+</div>
+<!-- {/if} -->
+
+<style>
+  .features{
+    border: solid;
+  }
+   p {
+    color: blue;
+    text-align: left;
+    padding-left: 10px;
+    max-width: 40%;
+  }
+  div {
+    margin: auto;
+    width: 95%;
+    border-width: 1px;
+    border-radius: 20px;
+    border-color: grey;
+    /* border: solid; */
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+  h1 {
+    color: blue;
+  }
+  h2 {
+    text-align: left;
+    padding-left: 10px;
+    color: #EB984E;
+    font-size: 2em;
+  }
+</style>
--- mistWebsite/src/components/Team.svelte
+++ mistWebsite/src/components/Team.svelte
@@ -1,87 +1,135 @@
 <script>
-  import assets from './assets';
+  /* import assets from './assets'; */
+
 </script>
 
 <div>
   <h1>Meet The Team!</h1>
 </div>
 
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <img src="" alt="Anthony" style="width:100%" />
-      <div class="container">
-        <h2>Anthony Marin</h2>
-        <p class="title">Software Engineer</p>
-        <a href="github.com/anthonymarinated">Github</a>
-        <a href="https://www.linkedin.com/in/anthmari/">LinkedIn</a>
-        <p>email</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
+<div class="row"> 
 
   <div class="column">
-    <div class="card">
-      <img src="" alt="Eric" style="width:100%" />
-      <div class="container">
-        <h2>Eric Lemay</h2>
-        <p class="title">Software Engineer</p>
-        <a href="github.com/StygianLiege">Github</a>
-        <a href="https://www.linkedin.com/in/eric-lemay-379659231/">LinkedIn</a>
-        <p>email</p>
-        <p><button class="button">Contact</button></p>
+        <div class="card">
+          <img src="" alt="Anthony" style="width:100%">
+          <div class="container">
+            <h2>Anthony Marin</h2>
+            <p class="title">Software Engineer</p>
+              <p>
+                <button class="button1">
+                  <a href="github.com/anthonymarinated">Github</a>
+                </button>
+              </p>
+              <p>
+                <button class="button2">
+                  <a href="https://www.linkedin.com/in/anthmari/">LinkedIn</a>
+                </button>
+              </p>
+              <a href="anthonymarin718@gmail.com">Email</a>          
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    
+      <div class="column">
+        <div class="card">
+          <img src="" alt="Eric" style="width:100%">
+          <div class="container">
+            <h2>Eric Lemay</h2>
+            <p class="title">Software Engineer</p>
+              <p>
+                <button class="button1">
+                  <a href="github.com/StygianLiege">Github</a>
+                </button>
+              </p>
+              <p>
+                <button class="button2">
+                  <a href="https://www.linkedin.com/in/eric-lemay-379659231/">LinkedIn</a>
+                </button>
+              </p>
+              <a href="https://www.linkedin.com/in/eric-lemay-379659231/" alt="alt">Email</a>          
+          </div>
+        </div>
+      </div>
 
-  <div class="column">
-    <div class="card">
-      <img src="" alt="Kristen" style="width:100%" />
-      <div class="container">
-        <h2>Kristen Althoff</h2>
-        <p class="title">Software Engineer</p>
-        <a href="https://github.com/KristenWAlthoff">Github</a>
-        <a href="https://www.linkedin.com/in/kristen-althoff-3a4765b9/">
-          LinkedIn"</a
-        >
-        <p>email</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
 
-  <div class="column">
-    <div class="card">
-      <img src="" alt="Leonoor" style="width:100%" />
-      <div class="container">
-        <h2>Leonoor Rinke de Wit</h2>
-        <p class="title">Software Engineer</p>
-        <a href="https://github.com/lrinkedewit">Github</a>
-        <a href="https://www.linkedin.com/in/leonoorrinkedewit/">LinkedIn</a>
-        <p>email</p>
-        <p><button class="button">Contact</button></p>
-      </div>
-    </div>
-  </div>
 
-  <div class="column">
-    <div class="card">
-      <img src="" alt="Stewart" style="width:100%" />
-      <div class="container">
-        <h2>Stewart Elmore</h2>
-        <p class="title">Software Engineer</p>
-        <a href="https://github.com/selmore7">Github</a>
-        <a href="https://www.linkedin.com/in/stewart-elmore/">LinkedIn</a>
-        <p>email</p>
-        <p><button class="button">Contact</button></p>
+      <div class="column">
+        <div class="card">
+          <img src="" alt="Kristen" style="width:100%">
+          <div class="container">
+            <h2>Kristen Althoff</h2>
+            <p class="title">Software Engineer</p>
+              <p>
+                <button class="button1">
+                  <a href="https://github.com/KristenWAlthoff">Github</a>
+                </button>
+              </p>
+              <p>
+                <button class="button2">
+                  <a href="https://www.linkedin.com/in/kristen-althoff-3a4765b9/">LinkedIn</a>
+                </button>
+              </p>
+              <a href="https://www.linkedin.com/in/kristen-althoff-3a4765b9/">Email</a>          
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+
+        
+      <div class="column">
+        <div class="card">
+          <img src="" alt="Leonoor" style="width:100%">
+          <div class="container">
+            <h2>Leonoor Rinke de Wit</h2>
+            <p class="title">Software Engineer</p>
+              <p>
+                <button class="button1">
+                  <a href="https://github.com/lrinkedewit">Github</a>
+                </button>
+              </p>
+              <p>
+                <button class="button2">
+                  <a href="https://www.linkedin.com/in/leonoorrinkedewit/">LinkedIn</a>
+                </button>
+              </p>
+              <a href="lrinkedewit@gmail.com">Email</a>          
+          </div>
+        </div>
+      </div>
+
+
+      <div class="column">
+        <div class="card">
+          <img src="" alt="Stewart" style="width:100%">
+          <div class="container">
+            <h2>Stewart Elmore</h2>
+            <p class="title">Software Engineer</p>
+              <p>
+                <button class="button1">
+                  <a href="https://github.com/selmore7">Github</a>
+                </button>
+              </p>
+              <p>
+                <button class="button2">
+                  <a href="https://www.linkedin.com/in/stewart-elmore/">LinkedIn</a>
+                </button>
+              </p>
+              <a href="stewart.elmore@gmail.com">Email</a>          
+          </div>
+        </div>
+      </div>  
+
+
 </div>
 
 <style>
-  /* columns side by side */
+  div.row{
+    margin: auto;
+  } 
+
+  h1{
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
   .column {
     float: left;
     width: auto;
@@ -113,7 +161,7 @@
   }
 
   .title {
-    color: blue;
+    color: rgb(90, 167, 218);
   }
 
   .button {
@@ -121,14 +169,21 @@
     outline: 0;
     display: inline-block;
     padding: 8px;
-    color: white;
-    background-color: #000;
     text-align: center;
     cursor: pointer;
     width: 100%;
   }
 
-  .button:hover {
+  .button1 {
+    background-color: black;
+    color:ohite;
+  }
+
+  .button2 {
     background-color: blue;
   }
+/* 
+  .button:hover {
+    background-color: blue;
+  } */
 </style>
